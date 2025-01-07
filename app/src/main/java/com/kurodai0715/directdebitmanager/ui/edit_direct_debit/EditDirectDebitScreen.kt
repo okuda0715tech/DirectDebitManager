3680b9e841d046918202c30f3b51c0b9bd0b9847
奥田 智紘 <okuda0715tech@gmail.com>
2025-01-07 11:34:52 +0900

    【リファクタリング】不要な import 文を削除した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/edit_direct_debit/EditDirectDebitScreen.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/edit_direct_debit/EditDirectDebitScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -14,11 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kurodai0715.directdebitmanager.R
-import com.kurodai0715.directdebitmanager.ui.component.DatePickerText
 import com.kurodai0715.directdebitmanager.ui.theme.SCREEN_EDGE_PADDING_DEF
 
 @Composable
