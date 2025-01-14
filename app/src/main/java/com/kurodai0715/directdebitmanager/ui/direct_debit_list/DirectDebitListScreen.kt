34f90e266073eaa7fc8d8ce6a1af60ab4278f02b
奥田 智紘 <okuda0715tech@gmail.com>
2025-01-14 18:32:48 +0900

    【リファクタリング】リスト画面を Scaffold 化しました。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/direct_debit_list/DirectDebitListScreen.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/direct_debit_list/DirectDebitListScreen.kt
@@ -4,17 +4,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -31,30 +37,44 @@ fun DirectDebitListScreen(
     viewModel: DirectDebitListViewModel = hiltViewModel(),
     onNavigateToEdit: (DirectDebit?) -> Unit,
 ) {
-    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(SCREEN_EDGE_PADDING_DEF)
-    ) {
-        LazyColumn(modifier = Modifier.weight(1f)) {
-            itemsIndexed(uiState.items) { index, item ->
-                val modifier = when (index) {
-                    // 最初のアイテムは Bottom にのみパディング
-                    0 -> Modifier.padding(bottom = 8.dp)
-                    // 最後のアイテムは Top にのみパディング
-                    uiState.items.size - 1 -> Modifier.padding(top = 8.dp)
-                    // それ以外のアイテムは Top と Bottom にパディング
-                    else -> Modifier.padding(vertical = 8.dp)
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    Scaffold(snackbarHost = {
+        SnackbarHost(
+            hostState = snackbarHostState,
+            // Snackbar がキーボードで隠れないようにする。
+            modifier = Modifier.safeDrawingPadding()
+        )
+    }) { paddingValues ->
+
+        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .consumeWindowInsets(paddingValues)
+                .padding(SCREEN_EDGE_PADDING_DEF)
+        ) {
+            LazyColumn(modifier = Modifier.weight(1f)) {
+                itemsIndexed(uiState.items) { index, item ->
+                    val modifier = when (index) {
+                        // 最初のアイテムは Bottom にのみパディング
+                        0 -> Modifier.padding(bottom = 8.dp)
+                        // 最後のアイテムは Top にのみパディング
+                        uiState.items.size - 1 -> Modifier.padding(top = 8.dp)
+                        // それ以外のアイテムは Top と Bottom にパディング
+                        else -> Modifier.padding(vertical = 8.dp)
+                    }
+                    DirectDebitItem(item, modifier, onClickItem = { onNavigateToEdit(item) })
                 }
-                DirectDebitItem(item, modifier, onClickItem = { onNavigateToEdit(item) })
             }
-        }
 
-        Button(onClick = { onNavigateToEdit(null) }) {
-            Text(stringResource(R.string.common_add))
+            Button(onClick = { onNavigateToEdit(null) }) {
+                Text(stringResource(R.string.common_add))
+            }
         }
     }
 }
