11315b0d106419860a66d24904ed0dcfeb8f0aef
奥田 智紘 <okuda0715tech@gmail.com>
2025-01-16 10:43:41 +0900

    【リファクタリング】不要なimport文を削除した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/MainActivity.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/MainActivity.kt
@@ -4,22 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Shapes
-import androidx.compose.material3.Text
-import androidx.compose.material3.Typography
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.kurodai0715.directdebitmanager.data.source.DirectDebit
 import com.kurodai0715.directdebitmanager.ui.app_base.AppBaseScreen
-import com.kurodai0715.directdebitmanager.ui.edit_direct_debit.EditDirectDebitScreen
 import com.kurodai0715.directdebitmanager.ui.theme.DirectDebitManagerTheme
 import dagger.hilt.android.AndroidEntryPoint
 
