891496de2638c4f8498b278dbe5bb19f2acf5186
奥田 智紘 <okuda0715tech@gmail.com>
2025-01-22 11:11:06 +0900

    【UI修正】ボタン位置の調整を行った。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/source_list/SourceListScreen.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/source_list/SourceListScreen.kt
@@ -1,9 +1,11 @@
 package com.kurodai0715.directdebitmanager.ui.source_list
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.kurodai0715.directdebitmanager.R
@@ -14,14 +16,21 @@ import com.kurodai0715.directdebitmanager.ui.util.debouncedClick
 fun SourceListScreen(
     onNavigateToEdit: (TransSource?) -> Unit,
 ) {
-    SourceListContents(onNavigateToEdit = onNavigateToEdit)
+    SourceListContents(
+        modifier = Modifier.fillMaxSize(),
+        onNavigateToEdit = onNavigateToEdit
+    )
 }
 
 @Composable
 fun SourceListContents(
+    modifier: Modifier = Modifier,
     onNavigateToEdit: (TransSource?) -> Unit,
 ) {
-    Column {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier,
+    ) {
         Button(onClick = { debouncedClick { onNavigateToEdit(null) } }) {
             Text(stringResource(R.string.common_add))
         }
