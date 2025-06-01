57dab489a714e6c929b512e699095af715afe087
奥田 智紘 <okuda0715tech@gmail.com>
2025-06-01 12:01:39 +0900

    【リファクタリング】SourceType クラスを domain パッケージに移動した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/data/source/Source.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/data/source/Source.kt
@@ -13,25 +13,3 @@ data class Source(
     val type: Int,
 )
 
-sealed class SourceType {
-    object Bank : SourceType()
-    object CreditCard : SourceType()
-    object DebitCard : SourceType()
-    object Others : SourceType()
-
-    companion object {
-        fun fromInt(value: Int): SourceType = when (value) {
-            0 -> Bank
-            1 -> CreditCard
-            2 -> DebitCard
-            else -> Others
-        }
-
-        fun toInt(sourceType: SourceType): Int = when (sourceType) {
-            Bank -> 0
-            CreditCard -> 1
-            DebitCard -> 2
-            Others -> 3
-        }
-    }
-}
