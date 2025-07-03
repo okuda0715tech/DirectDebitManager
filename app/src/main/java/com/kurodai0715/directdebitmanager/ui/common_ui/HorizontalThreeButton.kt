5440d0455f23dd5c6f386b9ae01cf03d7306d379
奥田 智紘 <okuda0715tech@gmail.com>
2025-07-03 19:21:53 +0900

    refactor: ファイル名を変更した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/common_ui/HorizontalThreeButton.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/common_ui/HorizontalThreeButton.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kurodai0715.directdebitmanager.R
-import com.kurodai0715.directdebitmanager.ui.common_ui.components.HorizontalTwoButton
 
 @Composable
 fun HorizontalThreeButton(
