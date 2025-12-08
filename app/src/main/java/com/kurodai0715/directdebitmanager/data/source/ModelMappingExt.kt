d5f4387e40279779d5401c27cf42990ca65c11ca
奥田 智紘 <okuda0715tech@gmail.com>
2025-12-08 16:21:03 +0900

    refactor: 未使用コードを削除した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/data/source/ModelMappingExt.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/data/source/ModelMappingExt.kt
@@ -1,41 +1,7 @@
 package com.kurodai0715.directdebitmanager.data.source
 
-import com.kurodai0715.directdebitmanager.data.source.local.LocalDestWithSource
-import com.kurodai0715.directdebitmanager.data.source.local.LocalDestination
-import com.kurodai0715.directdebitmanager.data.source.local.LocalSource
 import com.kurodai0715.directdebitmanager.data.source.local.LocalTransferItem
 import com.kurodai0715.directdebitmanager.domain.TransferItemType
-import com.kurodai0715.directdebitmanager.ui.screen.destination_edit.DestinationUiModel
-import com.kurodai0715.directdebitmanager.ui.screen.destination_list.DestWithSourceUiModel
-
-fun LocalDestination.toExternal() = DestinationUiModel(
-    id = id,
-    name = name,
-    sourceId = sourceId,
-//    date = date,
-//    amount = amount.toString(),
-)
-
-fun DestinationUiModel.toLocal() = LocalDestination(
-    id = id,
-    name = name,
-    sourceId = sourceId,
-//    date = date,
-//    amount = amount.toString(),
-)
-
-fun Source.toLocal() = LocalSource(
-    id = id,
-    name = name,
-    type = type,
-)
-
-fun LocalDestWithSource.toExternal() = DestWithSourceUiModel(
-    destId = destId,
-    destName = destName,
-    sourceId = sourceId,
-    sourceName = sourceName,
-)
 
 fun LocalTransferItem.toSource(): Source {
     val type = requireNotNull(type) { "LocalTransferItem.type is null" }
@@ -47,22 +13,6 @@ fun LocalTransferItem.toSource(): Source {
         type = type,
     )
 }
-
-fun Source.toLocalTransferItem() = LocalTransferItem(
-    id = id,
-    label = name,
-    isSourceItem = true,
-    type = type,
-    parentId = 0,
-)
-
-fun DestinationUiModel.toLocalTransferItem() = LocalTransferItem(
-    id = id,
-    label = name,
-    isSourceItem = false, // TODO [Destination] クラスに isSourceItem 項目を追加して、それを渡すように修正する
-    type = null,
-    parentId = sourceId,
-)
 
 fun LocalTransferItem.toExternal() = TransferItem(
     id = id,
