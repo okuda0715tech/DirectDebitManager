6c03db5f7582cfd0bc38e20a930159a14fc59ef2
奥田 智紘 <okuda0715tech@gmail.com>
2025-12-10 11:31:58 +0900

    refactor: 関数をインライン化した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/navigation/AppNavGraph.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/navigation/AppNavGraph.kt
@@ -5,13 +5,26 @@
 
 package com.kurodai0715.directdebitmanager.ui.navigation
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.kurodai0715.directdebitmanager.R
+import com.kurodai0715.directdebitmanager.R.string.edit_screen_title
+import com.kurodai0715.directdebitmanager.R.string.register_screen_title
+import com.kurodai0715.directdebitmanager.R.string.source_list_title
+import com.kurodai0715.directdebitmanager.R.string.source_registration_title
+import com.kurodai0715.directdebitmanager.R.string.source_update_title
+import com.kurodai0715.directdebitmanager.ui.screen.destination_edit.DestinationEditScreen
+import com.kurodai0715.directdebitmanager.ui.screen.destination_list.DestinationListScreen
+import com.kurodai0715.directdebitmanager.ui.screen.source_edit.SourceEditScreen
+import com.kurodai0715.directdebitmanager.ui.screen.source_list.SourceListScreen
 
-//private const val TAG = "AppNavGraph.kt"
+private const val TAG = "AppNavGraph.kt"
 
 @Composable
 fun AppNavGraph(
@@ -26,41 +39,56 @@ fun AppNavGraph(
         modifier = modifier,
     ) {
 
-        destListDestination(
-            onNavigateToEdit = { selectedItem ->
-                navController.navigateToDestEdit(selectedItem)
-            },
-            onChangeTitle = onChangeTitle
-        )
+        composable<DestList> {
+            DestinationListScreen(
+                onNavigateToEdit = { navController.navigateToDestEdit(it) }
+            )
+            onChangeTitle(R.string.list_screen_title)
+        }
 
-        destEditDestination(
-            onNavigateUp = {
-                navController.navigateUp()
-            },
-            onChangeTitle = onChangeTitle,
-            onNavigateToSourceList = {
-                navController.navigateToSourceList()
-            },
-            onNavigateToSourceEdit = {
-                navController.navigateToSourceEdit(null)
-            }
-        )
+        composable<DestEdit> { backStackEntry ->
+            val destEdit: DestEdit = backStackEntry.toRoute()
+            DestinationEditScreen(
+                destinationId = destEdit.destId,
+                onNavigateUp = { navController.navigateUp() },
+                onNavigateToSourceList = { navController.navigateToSourceList() },
+                onNavigateToSourceEdit = { navController.navigateToSourceEdit(null) },
+            )
 
-        sourceListDestination(
-            onNavigateUp = {
-                navController.navigateUp()
-            },
-            onNavigateToEdit = { selectedItemId ->
-                navController.navigateToSourceEdit(selectedItemId)
-            },
-            onChangeTitle = onChangeTitle
-        )
+            Log.d(TAG, "edit.id = ${destEdit.destId}")
 
-        sourceEditDestination(
-            onNavigateUp = {
-                navController.navigateUp()
-            },
-            onChangeTitle = onChangeTitle
-        )
+            onChangeTitle(
+                if (destEdit.destId == null)
+                    register_screen_title
+                else
+                    edit_screen_title
+            )
+        }
+
+        composable<SourceList> {
+            SourceListScreen(
+                onNavigateUp = { navController.navigateUp() },
+                onNavigateToEdit = { navController.navigateToSourceEdit(it) }
+            )
+            onChangeTitle(source_list_title)
+        }
+
+        composable<SourceEdit> { backStackEntry ->
+            val sourceEdit: SourceEdit = backStackEntry.toRoute()
+
+            SourceEditScreen(
+                sourceId = sourceEdit.sourceId,
+                onNavigateUp = { navController.navigateUp() },
+            )
+
+            Log.d(TAG, "sourceEdit.id = ${sourceEdit.sourceId}")
+
+            onChangeTitle(
+                if (sourceEdit.sourceId == null)
+                    source_registration_title
+                else
+                    source_update_title
+            )
+        }
     }
 }
--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/navigation/AppNavigation.kt
@@ -5,17 +5,8 @@
 
 package com.kurodai0715.directdebitmanager.ui.navigation
 
-import android.util.Log
 import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.compose.composable
-import androidx.navigation.toRoute
-import com.kurodai0715.directdebitmanager.R
-import com.kurodai0715.directdebitmanager.ui.screen.destination_edit.DestinationEditScreen
 import com.kurodai0715.directdebitmanager.ui.screen.destination_list.DestWithSourceUiModel
-import com.kurodai0715.directdebitmanager.ui.screen.destination_list.DestinationListScreen
-import com.kurodai0715.directdebitmanager.ui.screen.source_edit.SourceEditScreen
-import com.kurodai0715.directdebitmanager.ui.screen.source_list.SourceListScreen
 import kotlinx.serialization.Serializable
 
 private const val TAG = "AppNavigation.kt"
@@ -35,79 +26,6 @@ data object SourceList
 data class SourceEdit(
     val sourceId: Int? = null,
 )
-
-fun NavGraphBuilder.destListDestination(
-    onNavigateToEdit: (DestWithSourceUiModel?) -> Unit,
-    onChangeTitle: (Int) -> Unit,
-) {
-    composable<DestList> {
-        DestinationListScreen(onNavigateToEdit = onNavigateToEdit)
-        onChangeTitle(R.string.list_screen_title)
-    }
-}
-
-fun NavGraphBuilder.destEditDestination(
-    onNavigateUp: () -> Unit,
-    onChangeTitle: (Int) -> Unit,
-    onNavigateToSourceList: () -> Unit,
-    onNavigateToSourceEdit: () -> Unit,
-) {
-    composable<DestEdit> { backStackEntry ->
-        val destEdit: DestEdit = backStackEntry.toRoute()
-        DestinationEditScreen(
-            destinationId = destEdit.destId,
-            onNavigateUp = onNavigateUp,
-            onNavigateToSourceList = onNavigateToSourceList,
-            onNavigateToSourceEdit = onNavigateToSourceEdit,
-        )
-
-        Log.d(TAG, "edit.id = ${destEdit.destId}")
-
-        onChangeTitle(
-            if (destEdit.destId == null)
-                R.string.register_screen_title
-            else
-                R.string.edit_screen_title
-        )
-    }
-}
-
-fun NavGraphBuilder.sourceListDestination(
-    onNavigateUp: () -> Unit,
-    onNavigateToEdit: (Int?) -> Unit,
-    onChangeTitle: (Int) -> Unit,
-) {
-    composable<SourceList> {
-        SourceListScreen(
-            onNavigateUp = onNavigateUp,
-            onNavigateToEdit = onNavigateToEdit
-        )
-        onChangeTitle(R.string.source_list_title)
-    }
-}
-
-fun NavGraphBuilder.sourceEditDestination(
-    onNavigateUp: () -> Unit,
-    onChangeTitle: (Int) -> Unit,
-) {
-    composable<SourceEdit> { backStackEntry ->
-        val sourceEdit: SourceEdit = backStackEntry.toRoute()
-
-        SourceEditScreen(
-            sourceId = sourceEdit.sourceId,
-            onNavigateUp = onNavigateUp,
-        )
-
-        Log.d(TAG, "sourceEdit.id = ${sourceEdit.sourceId}")
-
-        onChangeTitle(
-            if (sourceEdit.sourceId == null)
-                R.string.source_registration_title
-            else
-                R.string.source_update_title
-        )
-    }
-}
 
 // TODO DestWithSourceUiModel への依存を解消する
 fun NavController.navigateToDestEdit(destWithSourceUiModel: DestWithSourceUiModel?) {
