f9cb1bec9b2ecb2439438384bb12307f7cac7811
奥田 智紘 <okuda0715tech@gmail.com>
2025-12-24 18:44:38 +0900

    docs: 余白戦略のコメントをより適切なものにした。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/theme/LayoutTokens.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/theme/LayoutTokens.kt
@@ -2,20 +2,33 @@ package com.kurodai0715.directdebitmanager.ui.theme
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.Dp
+import com.kurodai0715.directdebitmanager.ui.theme.LayoutTokens.itemSpacing
 
 object LayoutTokens {
 
     /**
-     * アイコンとラベルの間、ラベルと入力欄の間など、一つのコンポーネント内の要素間の余白.
+     * 一つのコンポーネント内の要素間の余白.
+     *
+     * 具体例 : アイコンとラベルの間、ラベルと入力欄の間など。
      */
     val elementSpacing: Dp
         @Composable get() = Spacing.s
 
     /**
      * コンポーネントとコンポーネントの間の余白.
+     *
+     * 具体例 : リストアイテムの要素間、フォームの項目同士の間など。
      */
     val itemSpacing: Dp
         @Composable get() = Spacing.s
+
+    /**
+     * [itemSpacing] の半分の余白.
+     *
+     * リストアイテムの要素間の余白は、隣り合う要素がそれぞれ余白を持つため、実際の余白の半分のプロパティがあると便利。
+     */
+    val itemSpacingHalf: Dp
+        @Composable get() = itemSpacing / 2
 
     /**
      * セクションとセクションの間の余白.
