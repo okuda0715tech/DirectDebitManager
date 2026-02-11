23887d97771dc1c13d79557e5a6c1301cd337e6d
奥田 智紘 <okuda0715tech@gmail.com>
2026-02-11 10:29:56 +0900

    docs: 新しく追加したファイルに著作権を記載した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/domain/usecase/SourcesCommandUseCase.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/domain/usecase/SourcesCommandUseCase.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2025 Okuda Tomohiro
+ * Licensed under the MIT License.
+ */
+
 package com.kurodai0715.directdebitmanager.domain.usecase
 
 class SourcesCommandUseCase {
--- app/src/main/java/com/kurodai0715/directdebitmanager/domain/usecase/SourcesQueryUseCase.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/domain/usecase/SourcesQueryUseCase.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2025 Okuda Tomohiro
+ * Licensed under the MIT License.
+ */
+
 package com.kurodai0715.directdebitmanager.domain.usecase
 
 import com.kurodai0715.directdebitmanager.data.DirectDebitDefaultRepository
