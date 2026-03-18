6b9ed8d91c305f29c1f6499372de6d742bf0285e
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-18 18:55:32 +0900

    feat: ホーム画面のUIを作成した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/home/HomeScreen.kt
@@ -1,13 +1,43 @@
 package com.kurodai0715.directdebitmanager.ui.screen.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.kurodai0715.directdebitmanager.R
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Button(onClick = {}) {
-        Text(text = "Home Screen")
+    HomeScreenContents(modifier)
+}
+
+@Composable
+fun HomeScreenContents(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Button(onClick = {}) {
+            Text(text = stringResource(R.string.payer_list_label))
+        }
+        Button(onClick = {}) {
+            Text(text = stringResource(R.string.payee_list_label))
+        }
+        Button(onClick = {}) {
+            Text(text = stringResource(R.string.relation_list_label))
+        }
     }
+}
+
+@Preview
+@Composable
+private fun PreviewHomeScreenContents() {
+    HomeScreenContents()
 }
