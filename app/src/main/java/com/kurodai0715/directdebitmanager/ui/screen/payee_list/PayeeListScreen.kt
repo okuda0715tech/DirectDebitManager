3c03de257ec79a7b63d2bb38a23309a1c7575978
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-20 16:54:29 +0900

    feat: 支払先一覧画面にリスト表示機構を作った。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payee_list/PayeeListScreen.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payee_list/PayeeListScreen.kt
@@ -1,9 +1,15 @@
 package com.kurodai0715.directdebitmanager.ui.screen.payee_list
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.kurodai0715.directdebitmanager.ui.theme.LayoutTokens
 
 @Composable
 fun PayeeListScreen(modifier: Modifier = Modifier) {
@@ -12,7 +18,15 @@ fun PayeeListScreen(modifier: Modifier = Modifier) {
 
 @Composable
 fun PayeeListContents(modifier: Modifier = Modifier) {
-    Text(text = "payee list", modifier = modifier)
+    LazyColumn(
+        modifier = modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(LayoutTokens.sectionSpacingHalf),
+        verticalArrangement = Arrangement.spacedBy(LayoutTokens.itemSpacing)
+    ) {
+        items(listOf("xxx", "yyy", "zzz")) { item ->
+            Text(text = item)
+        }
+    }
 }
 
 @Preview
