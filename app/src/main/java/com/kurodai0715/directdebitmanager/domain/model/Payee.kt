ae75e000ca0872ede1f8c45a024654f79056b163
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-21 11:53:50 +0900

    feat: Payee インスタンス生成関数を定義した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/domain/model/Payee.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/domain/model/Payee.kt
@@ -1,6 +1,20 @@
+// TODO 著作権の宣言
+
 package com.kurodai0715.directdebitmanager.domain.model
 
+// TODO プライマリコンストラクタを private にして、 create 関数経由でのみインスタンスの生成を許可する。
 data class Payee(
     val id: Int,
     val name: String,
-)
+) {
+    companion object {
+        fun create(
+            id: Int,
+            label: String,
+        ): Payee {
+            require(label.isNotBlank()) { "label is blank" }
+
+            return Payee(id, label)
+        }
+    }
+}
