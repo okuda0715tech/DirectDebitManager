6bd01e573608299904ce810dc4043f6a19cc3bb6
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-21 10:48:17 +0900

    feat: 支払先登録画面の画面全体の余白調整を行った。スナックバーの表示準備も一部実施した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/register_payee/RegisterPayeeScreen.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/register_payee/RegisterPayeeScreen.kt
@@ -1,8 +1,15 @@
 package com.kurodai0715.directdebitmanager.ui.screen.register_payee
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -11,20 +18,35 @@ import com.kurodai0715.directdebitmanager.R
 import com.kurodai0715.directdebitmanager.ui.common_ui.components.EditableForm
 import com.kurodai0715.directdebitmanager.ui.common_ui.components.HorizontalTwoButton
 import com.kurodai0715.directdebitmanager.ui.common_ui.screens.ContentsWithBottomButton
+import com.kurodai0715.directdebitmanager.ui.theme.LayoutTokens
 import com.kurodai0715.directdebitmanager.ui.util.debouncedClick
 
 @Composable
 fun RegisterPayeeScreen(
     viewModel: RegisterPayeeViewModel = hiltViewModel(),
 ) {
-    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val snackbarHostState = remember { SnackbarHostState() }
 
-    RegisterPayeeContents(
-        modifier = Modifier,
-        payeeName = uiState.payeeName,
-        onPayeeNameChanged = { viewModel.updatePayeeName(it) },
-        supportingTextRes = null
-    )
+    Scaffold(snackbarHost = {
+        SnackbarHost(
+            hostState = snackbarHostState,
+            // Snackbar がキーボードで隠れないようにする。
+            modifier = Modifier.safeDrawingPadding()
+        )
+    }) { paddingValues ->
+
+        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+        RegisterPayeeContents(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(LayoutTokens.screenPaddingHalf),
+            payeeName = uiState.payeeName,
+            onPayeeNameChanged = { viewModel.updatePayeeName(it) },
+            supportingTextRes = null
+        )
+    }
 }
 
 @Composable
