ac26d032c4b2149de9f0da5695e764f51296efc7
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-23 17:18:11 +0900

    refactor: 不要になったクラスを削除した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/domain/model/Payee.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/domain/model/Payee.kt
@@ -1,10 +1,5 @@
 package com.kurodai0715.directdebitmanager.domain.model
 
-data class Payee(
-    val id: Int,
-    val name: PayeeName,
-)
-
 @JvmInline
 value class PayeeName(val value: String) {
     init {
