fda59512ded17f90fbe9d2bec36ecb0b59a33607
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-23 17:17:45 +0900

    refactor: 既存の支払先モデル(Payee)を使用している部分を新たな支払先モデル(Payee2)に置き換えた。

--- app/src/main/java/com/kurodai0715/directdebitmanager/domain/usecase/PayeeQueryUseCase.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/domain/usecase/PayeeQueryUseCase.kt
@@ -2,7 +2,7 @@ package com.kurodai0715.directdebitmanager.domain.usecase
 
 import com.kurodai0715.directdebitmanager.data.DirectDebitDefaultRepository
 import com.kurodai0715.directdebitmanager.domain.mapper.toPayee
-import com.kurodai0715.directdebitmanager.domain.model.Payee
+import com.kurodai0715.directdebitmanager.domain.model.Payee2
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -11,13 +11,13 @@ class PayeeQueryUseCase @Inject constructor(
     private val repo: DirectDebitDefaultRepository
 ) {
 
-    fun loadPayees(): Flow<List<Payee>> {
+    fun loadPayees(): Flow<List<Payee2.Persisted>> {
         return repo.observePayees().map { list ->
             list.map { it.toPayee() }
         }
     }
 
-    suspend fun loadPayeeBy(payeeId: Int): Payee {
+    suspend fun loadPayeeBy(payeeId: Int): Payee2.Persisted {
         return repo.loadItem(payeeId).toPayee()
     }
 
--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payee_list/PayeeListUiMapper.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payee_list/PayeeListUiMapper.kt
@@ -1,14 +1,14 @@
 package com.kurodai0715.directdebitmanager.ui.screen.payee_list
 
-import com.kurodai0715.directdebitmanager.domain.model.Payee
+import com.kurodai0715.directdebitmanager.domain.model.Payee2
 
-fun Payee.toPayeeUiModel(): PayeeUiModel {
+fun Payee2.Persisted.toPayeeUiModel(): PayeeUiModel {
     return PayeeUiModel(
         id = id,
         name = name.value,
     )
 }
 
-fun List<Payee>.toPayeeUiModels(): List<PayeeUiModel>{
+fun List<Payee2.Persisted>.toPayeeUiModels(): List<PayeeUiModel>{
     return map { it.toPayeeUiModel() }
 }
