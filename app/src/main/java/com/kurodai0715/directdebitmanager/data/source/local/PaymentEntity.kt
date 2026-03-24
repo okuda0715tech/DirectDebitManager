0b3a18689f4e7ada6bf932557a9499d2481f385b
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-24 17:35:07 +0900

    feat: TransferItemEntity をコピーして PaymentEntity を定義した。

---
+++ app/src/main/java/com/kurodai0715/directdebitmanager/data/source/local/PaymentEntity.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2025 Okuda Tomohiro
+ * Licensed under the MIT License.
+ */
+
+package com.kurodai0715.directdebitmanager.data.source.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.kurodai0715.directdebitmanager.domain.model.ItemType
+
+@Entity(tableName = "transfer_item")
+data class PaymentEntity(
+
+    /**
+     * ID.
+     *
+     * ID に 0 が設定されたオブジェクトが Room の @Insert で挿入されると、
+     * ID は自動採番されます。
+     */
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    /**
+     * 振替元、もしくは、振替先の名称.
+     */
+    val label: String,
+
+    /**
+     * この  [PaymentEntity] が振替元として登録されたかものどうかを判別するためのもの.
+     *
+     * 振替元として登録された場合は true 。そうでなければ false 。
+     */
+    val isSourceItem: Boolean,
+
+    /**
+     * この [PaymentEntity] の区分.
+     *
+     * 具体的な区分の種類は、 [ItemType] を参照。
+     */
+    val typeCode: Int?,
+
+    /**
+     * この [PaymentEntity] の振替元の [PaymentEntity] の ID .
+     *
+     * 0 の場合、この [PaymentEntity] は、振替元を持たない（つまり、ルートである）ことを意味する。
+     */
+    val parentId: Int = 0,
+
+    val role: PaymentRole,
+
+//    val date: String,
+//    val amount: Int,
+
+)
