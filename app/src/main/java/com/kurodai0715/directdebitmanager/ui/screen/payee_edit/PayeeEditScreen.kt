3c253e9b070a89b9db6251b335e43d8b2d8e2e75
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-24 15:52:09 +0900

    feat: 支払先編集画面のプレビューを Android Studio 上で確認できるようにした。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payee_edit/PayeeEditScreen.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payee_edit/PayeeEditScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kurodai0715.directdebitmanager.R
@@ -153,4 +154,18 @@ fun Contents(
             onClickClear = { onPayeeNameChanged("") }
         )
     }
+}
+
+@Preview
+@Composable
+private fun PreviewPayeeEditContents() {
+    PayeeEditContents(
+        mode = PayeeEditMode.Add,
+        payeeName = "",
+        onPayeeNameChanged = {},
+        onClickBack = {},
+        onClickSave = {},
+        supportingTextRes = null,
+    )
+    
 }
