30a636d2db627a50e9b625aead7a32e94ee0c7ad
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-26 14:46:27 +0900

    refactor: 不要な import 文を削除した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/domain/mapper/PayeeMapper.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/domain/mapper/PayeeMapper.kt
@@ -1,7 +1,6 @@
 package com.kurodai0715.directdebitmanager.domain.mapper
 
 import com.kurodai0715.directdebitmanager.data.source.local.PaymentItemEntity
-import com.kurodai0715.directdebitmanager.data.source.local.TransferItemEntity
 import com.kurodai0715.directdebitmanager.domain.model.Payee
 import com.kurodai0715.directdebitmanager.domain.model.PayeeName
 
