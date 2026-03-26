5fa51c0196754d13e5f21fe518f990170cdb9061
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-26 14:59:58 +0900

    feat(UI): 振替関係一覧画面の枠を定義した。

---
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/transfer_relation_list/TransferRelationListScreen.kt
@@ -0,0 +1,11 @@
+package com.kurodai0715.directdebitmanager.ui.screen.transfer_relation_list
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TransferRelationListScreen(modifier: Modifier = Modifier) {
+    Text(text = "Transfer Relation Contents")
+}
+
