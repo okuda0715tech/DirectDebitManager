f573e8e3d2f0956976c4704f4d8b13b866e3f414
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-26 15:19:17 +0900

    feat(UI): 振替関係一覧画面に追加ボタンと戻るボタンを追加した。（動作は未実装）

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/transfer_relation_list/TransferRelationListScreen.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/transfer_relation_list/TransferRelationListScreen.kt
@@ -1,11 +1,38 @@
 package com.kurodai0715.directdebitmanager.ui.screen.transfer_relation_list
 
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.kurodai0715.directdebitmanager.R
+import com.kurodai0715.directdebitmanager.ui.common_ui.components.HorizontalTwoButton
+import com.kurodai0715.directdebitmanager.ui.common_ui.screens.ContentsWithBottomButton
+import com.kurodai0715.directdebitmanager.ui.util.debouncedClick
 
 @Composable
 fun TransferRelationListScreen(modifier: Modifier = Modifier) {
-    Text(text = "Transfer Relation Contents")
+    TransferRelationListContents(modifier)
 }
 
+@Composable
+fun TransferRelationListContents(modifier: Modifier = Modifier) {
+    ContentsWithBottomButton(
+        modifier = modifier,
+        contents = {
+            Contents()
+        },
+        bottomButton = {
+            HorizontalTwoButton(
+                onClickLeft = { debouncedClick { TODO() } },
+                onClickRight = { debouncedClick { TODO() } },
+                leftText = stringResource(R.string.common_back),
+                rightText = stringResource(R.string.common_add)
+            )
+        }
+    )
+}
+
+@Composable
+fun Contents() {
+    Text(text = "Contents")
+}
