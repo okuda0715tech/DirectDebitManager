4c10748c8119627adf8bc5318fd3b9dea680c1e3
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-27 20:25:12 +0900

    feat(UI): 支払情報選択画面のプレビューを作成した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/select_payment/SelectPaymentScreen.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/select_payment/SelectPaymentScreen.kt
@@ -3,6 +3,7 @@ package com.kurodai0715.directdebitmanager.ui.screen.select_payment
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun SelectPaymentScreen(modifier: Modifier = Modifier) {
@@ -12,4 +13,10 @@ fun SelectPaymentScreen(modifier: Modifier = Modifier) {
 @Composable
 fun SelectPaymentContents() {
     Text(text = "SelectPaymentContents")
+}
+
+@Preview(name = "SelectPaymentScreen")
+@Composable
+private fun Preview() {
+    SelectPaymentContents()
 }
