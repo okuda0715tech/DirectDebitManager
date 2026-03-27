e1c974b5d2acbafe00f3e58551cc3f2a55903665
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-27 19:40:31 +0900

    feat: 支払情報編集画面の支払情報の名称 UI を作成した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_info_edit/PaymentInfoEditScreen.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_info_edit/PaymentInfoEditScreen.kt
@@ -1,12 +1,15 @@
 package com.kurodai0715.directdebitmanager.ui.screen.payment_info_edit
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kurodai0715.directdebitmanager.R
+import com.kurodai0715.directdebitmanager.ui.common_ui.components.EditableForm
 import com.kurodai0715.directdebitmanager.ui.common_ui.components.HorizontalTwoButton
 import com.kurodai0715.directdebitmanager.ui.common_ui.screens.ContentsWithBottomButton
 import com.kurodai0715.directdebitmanager.ui.util.debouncedClick
@@ -16,8 +19,14 @@ fun PaymentInfoEditScreen(
     viewModel: PaymentInfoEditViewModel = hiltViewModel(),
     onClickBack: () -> Unit,
 ) {
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
     TransferRelationEditContents(
-        onClickBack = onClickBack
+        onClickBack = onClickBack,
+        paymentName = uiState.paymentName,
+        onPaymentNameChanged = { viewModel.updatePaymentName(it) },
+        supportingTextRes = uiState.paymentNameErrorMessage,
     )
 }
 
@@ -25,11 +34,18 @@ fun PaymentInfoEditScreen(
 fun TransferRelationEditContents(
     modifier: Modifier = Modifier,
     onClickBack: () -> Unit,
+    paymentName: String,
+    onPaymentNameChanged: (String) -> Unit,
+    supportingTextRes: Int?
 ) {
     ContentsWithBottomButton(
         modifier = modifier,
         contents = {
-            Contents()
+            Contents(
+                paymentName = paymentName,
+                onPaymentNameChanged = onPaymentNameChanged,
+                supportingTextRes = supportingTextRes,
+            )
         },
         bottomButton = {
             HorizontalTwoButton(
@@ -44,14 +60,29 @@ fun TransferRelationEditContents(
 }
 
 @Composable
-fun Contents() {
-    Text(text = "Contents")
+fun Contents(
+    paymentName: String,
+    onPaymentNameChanged: (String) -> Unit,
+    supportingTextRes: Int?
+) {
+    Column {
+        EditableForm(
+            labelText = stringResource(R.string.payment_name_label),
+            text = paymentName,
+            onTextChanged = onPaymentNameChanged,
+            supportingText = supportingTextRes,
+            onClickClear = { onPaymentNameChanged("") }
+        )
+    }
 }
 
 @Preview(name = "TransferRelationEditContents")
 @Composable
 private fun Preview() {
     TransferRelationEditContents(
-        onClickBack = {}
+        onClickBack = {},
+        paymentName = "三井住友銀行",
+        onPaymentNameChanged = {},
+        supportingTextRes = null,
     )
 }
--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_info_edit/PaymentInfoEditViewModel.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_info_edit/PaymentInfoEditViewModel.kt
@@ -2,9 +2,38 @@ package com.kurodai0715.directdebitmanager.ui.screen.payment_info_edit
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
+
+data class PaymentInfoEditUiState(
+    val paymentName: String = "",
+    val paymentNameErrorMessage: Int? = null,
+)
+
 
 @HiltViewModel
 class PaymentInfoEditViewModel @Inject constructor(
 
-): ViewModel()
+) : ViewModel() {
+
+    /**
+     * 更新用.
+     */
+    private val _uiState = MutableStateFlow(PaymentInfoEditUiState())
+
+    /**
+     * 読み取り専用.
+     */
+    val uiState: StateFlow<PaymentInfoEditUiState> = _uiState.asStateFlow()
+
+    fun updatePaymentName(paymentName: String) {
+        _uiState.update {
+            it.copy(
+                paymentName = paymentName
+            )
+        }
+    }
+}
