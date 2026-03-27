33c0302d17f23328e163893d9b5a1138eacae7c1
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-27 20:24:03 +0900

    feat(UI): 支払情報選択画面の枠を定義した。

---
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/select_payment/SelectPaymentScreen.kt
@@ -0,0 +1,14 @@
+package com.kurodai0715.directdebitmanager.ui.screen.select_payment
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SelectPaymentScreen(modifier: Modifier = Modifier) {
+    SelectPaymentContents()
+}
+
+@Composable
+fun SelectPaymentContents() {
+    TODO("Not yet implemented")
+}
