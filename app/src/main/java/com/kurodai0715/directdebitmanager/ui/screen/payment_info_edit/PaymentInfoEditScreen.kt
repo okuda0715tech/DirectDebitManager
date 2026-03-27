6b619390c53a079af644f51a4af870e863b455e3
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-27 19:18:37 +0900

    feat: 支払情報編集画面にボタンを配置した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_info_edit/PaymentInfoEditScreen.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_info_edit/PaymentInfoEditScreen.kt
@@ -3,24 +3,42 @@ package com.kurodai0715.directdebitmanager.ui.screen.payment_info_edit
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.kurodai0715.directdebitmanager.R
+import com.kurodai0715.directdebitmanager.ui.common_ui.components.HorizontalTwoButton
+import com.kurodai0715.directdebitmanager.ui.common_ui.screens.ContentsWithBottomButton
+import com.kurodai0715.directdebitmanager.ui.util.debouncedClick
 
 @Composable
 fun PaymentInfoEditScreen(
-    modifier: Modifier = Modifier,
     viewModel: PaymentInfoEditViewModel = hiltViewModel()
 ) {
-    TransferRelationEditContents(modifier)
+    TransferRelationEditContents()
 }
 
 @Composable
 fun TransferRelationEditContents(modifier: Modifier = Modifier) {
-    Contents(modifier)
+    ContentsWithBottomButton(
+        modifier = modifier,
+        contents = {
+            Contents()
+        },
+        bottomButton = {
+            HorizontalTwoButton(
+                onClickLeft = { debouncedClick(TODO()) },
+                onClickRight = { debouncedClick(TODO()) },
+                leftText = stringResource(R.string.common_back),
+                rightText = stringResource(R.string.common_save)
+            )
+        }
+    )
+
 }
 
 @Composable
-fun Contents(modifier: Modifier = Modifier) {
+fun Contents() {
     Text(text = "Contents")
 }
 
