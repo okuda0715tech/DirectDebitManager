73381f286dea38e2acf848fda1723479c387f67d
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-28 13:32:29 +0900

    feat: ViewModelに保存ボタンがタップされた際に実行する処理を定義した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_info_edit/PaymentInfoEditViewModel.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_info_edit/PaymentInfoEditViewModel.kt
@@ -1,12 +1,16 @@
 package com.kurodai0715.directdebitmanager.ui.screen.payment_info_edit
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kurodai0715.directdebitmanager.domain.model.Payment
+import com.kurodai0715.directdebitmanager.domain.model.SaveResult
 import com.kurodai0715.directdebitmanager.domain.usecase.PaymentCommandUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 data class PaymentInfoEditUiState(
@@ -39,4 +43,17 @@ class PaymentInfoEditViewModel @Inject constructor(
             )
         }
     }
+
+    fun save() {
+        viewModelScope.launch {
+            val payment = uiState.value.toDomain()
+
+            val result = savePayment(payment)
+        }
+    }
+
+    private suspend fun savePayment(payment: Payment): SaveResult {
+        return paymentCommandUseCase.savePayment(payment)
+    }
+
 }
