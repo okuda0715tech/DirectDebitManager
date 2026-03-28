8bfba1f008f6b415c59c8bff396352e195b0d649
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-28 10:55:31 +0900

    feat: PaymentSelectScreen に ViewModel を接続した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_select/PaymentSelectScreen.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_select/PaymentSelectScreen.kt
@@ -3,9 +3,12 @@ package com.kurodai0715.directdebitmanager.ui.screen.payment_select
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun PaymentSelectScreen() {
+fun PaymentSelectScreen(
+    viewModel: PaymentSelectViewModel = hiltViewModel(),
+) {
     PaymentSelectContents()
 }
 
---
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_select/PaymentSelectViewModel.kt
@@ -0,0 +1,8 @@
+package com.kurodai0715.directdebitmanager.ui.screen.payment_select
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class PaymentSelectViewModel @Inject constructor(
+
+) : ViewModel()
