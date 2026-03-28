802dfefd4550e794eb3294ccdfde237bc0f87d51
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-28 12:22:57 +0900

    feat: リポジトリに全件取得処理を実装した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/data/PaymentV2DefaultRepository.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/data/PaymentV2DefaultRepository.kt
@@ -1,13 +1,19 @@
 package com.kurodai0715.directdebitmanager.data
 
+import com.kurodai0715.directdebitmanager.data.source.local.PaymentEntityV2
 import com.kurodai0715.directdebitmanager.data.source.local.PaymentV2Dao
 import com.kurodai0715.directdebitmanager.di.IoDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class PaymentV2DefaultRepository @Inject constructor(
     private val localDataSource: PaymentV2Dao,
     @param:IoDispatcher private val ioDispatcher: CoroutineDispatcher,
 ) : PaymentV2Repository {
+
+    fun loadPayments(): Flow<List<PaymentEntityV2>> {
+        return localDataSource.observePayments()
+    }
 
 }
