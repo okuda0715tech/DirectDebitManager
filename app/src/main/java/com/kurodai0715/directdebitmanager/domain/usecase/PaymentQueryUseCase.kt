ebbf649c6c003780a8e4d2c32d96d8b23ed28ee0
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-28 12:51:06 +0900

    feat: ユースケースからリポジトリの関数を呼び出す処理を実装した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/domain/usecase/PaymentQueryUseCase.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/domain/usecase/PaymentQueryUseCase.kt
@@ -1,9 +1,16 @@
 package com.kurodai0715.directdebitmanager.domain.usecase
 
 import com.kurodai0715.directdebitmanager.data.PaymentV2Repository
+import com.kurodai0715.directdebitmanager.data.source.local.PaymentEntityV2
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class PaymentQueryUseCase @Inject constructor(
     private val repo: PaymentV2Repository
 ) {
+
+    fun loadPayments(): Flow<List<PaymentEntityV2>> {
+        return repo.loadPayments()
+    }
+
 }
