733a8e712c4e9dabbc322383b81248fb32580db4
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-29 09:37:21 +0900

    feat(UI): リストのアイテム間の余白を削除した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/transfer_relation_list/TransferRelationListScreen.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/transfer_relation_list/TransferRelationListScreen.kt
@@ -1,7 +1,14 @@
 package com.kurodai0715.directdebitmanager.ui.screen.transfer_relation_list
 
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -12,9 +19,11 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kurodai0715.directdebitmanager.R
 import com.kurodai0715.directdebitmanager.ui.common_ui.components.HorizontalTwoButton
-import com.kurodai0715.directdebitmanager.ui.common_ui.elements.DefaultListItemFrame
 import com.kurodai0715.directdebitmanager.ui.common_ui.screens.ContentsWithBottomButton
+import com.kurodai0715.directdebitmanager.ui.theme.LayoutTokens
 import com.kurodai0715.directdebitmanager.ui.util.debouncedClick
+
+private const val TAG = "TransferRelationListScreen"
 
 @Composable
 fun TransferRelationListScreen(
@@ -74,7 +83,16 @@ fun ListItem(
     item: TransferRelationListUiState.Item,
     onClickItem: () -> Unit
 ) {
-    DefaultListItemFrame(onClickItem = onClickItem) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surfaceContainerLow)
+            .clickable(onClick = {
+                Log.v(TAG, "list item is clicked.")
+                debouncedClick(onClickItem)
+            })
+            .padding(LayoutTokens.elementSpacing)
+    ) {
         Text(text = item.name)
     }
 }
