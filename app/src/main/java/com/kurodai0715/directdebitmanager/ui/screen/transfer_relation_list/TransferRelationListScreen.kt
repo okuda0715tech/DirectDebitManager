712433d6907d5b493d332092e2858796c0747de4
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-29 09:31:08 +0900

    feat: UI状態にセットされたデータを画面に一覧表示する機能を実装した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/transfer_relation_list/TransferRelationListScreen.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/transfer_relation_list/TransferRelationListScreen.kt
@@ -1,5 +1,7 @@
 package com.kurodai0715.directdebitmanager.ui.screen.transfer_relation_list
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -10,6 +12,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kurodai0715.directdebitmanager.R
 import com.kurodai0715.directdebitmanager.ui.common_ui.components.HorizontalTwoButton
+import com.kurodai0715.directdebitmanager.ui.common_ui.elements.DefaultListItemFrame
 import com.kurodai0715.directdebitmanager.ui.common_ui.screens.ContentsWithBottomButton
 import com.kurodai0715.directdebitmanager.ui.util.debouncedClick
 
@@ -25,6 +28,8 @@ fun TransferRelationListScreen(
     TransferRelationListContents(
         onClickBack = onClickBack,
         onClickAdd = onClickAdd,
+        payments = uiState.payments,
+        onClickItem = { TODO() }
     )
 }
 
@@ -33,11 +38,13 @@ fun TransferRelationListContents(
     modifier: Modifier = Modifier,
     onClickBack: () -> Unit,
     onClickAdd: () -> Unit,
+    payments: List<TransferRelationListUiState.Item>,
+    onClickItem: (TransferRelationListUiState.Item) -> Unit
 ) {
     ContentsWithBottomButton(
         modifier = modifier,
         contents = {
-            Contents()
+            Contents(payments, onClickItem)
         },
         bottomButton = {
             HorizontalTwoButton(
@@ -51,9 +58,27 @@ fun TransferRelationListContents(
 }
 
 @Composable
-fun Contents() {
-    Text(text = "Contents")
+fun Contents(
+    payments: List<TransferRelationListUiState.Item>,
+    onClickItem: (TransferRelationListUiState.Item) -> Unit
+) {
+    LazyColumn {
+        items(payments) { item ->
+            ListItem(item, onClickItem = { onClickItem(item) })
+        }
+    }
 }
+
+@Composable
+fun ListItem(
+    item: TransferRelationListUiState.Item,
+    onClickItem: () -> Unit
+) {
+    DefaultListItemFrame(onClickItem = onClickItem) {
+        Text(text = item.name)
+    }
+}
+
 
 @Preview
 @Composable
@@ -61,5 +86,14 @@ private fun Preview() {
     TransferRelationListContents(
         onClickBack = { },
         onClickAdd = { },
+        payments = listOf(
+            TransferRelationListUiState.Item("テスト1"),
+            TransferRelationListUiState.Item("テスト2"),
+            TransferRelationListUiState.Item("テスト3"),
+            TransferRelationListUiState.Item("テスト4"),
+            TransferRelationListUiState.Item("テスト5"),
+            TransferRelationListUiState.Item("テスト6"),
+        ),
+        onClickItem = { }
     )
 }
