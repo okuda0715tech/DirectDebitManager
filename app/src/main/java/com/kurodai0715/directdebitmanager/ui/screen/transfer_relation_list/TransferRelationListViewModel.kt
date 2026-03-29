8988fad31a342e293c3b228bb9a30f10e4721861
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-29 09:15:58 +0900

    feat: 取得したデータをUI状態にセットする処理を実装した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/transfer_relation_list/TransferRelationListViewModel.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/transfer_relation_list/TransferRelationListViewModel.kt
@@ -1,19 +1,22 @@
 package com.kurodai0715.directdebitmanager.ui.screen.transfer_relation_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kurodai0715.directdebitmanager.R
 import com.kurodai0715.directdebitmanager.domain.usecase.PaymentQueryUseCase
 import com.kurodai0715.directdebitmanager.ui.util.Async
+import com.kurodai0715.directdebitmanager.ui.util.WhileUiSubscribed
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 data class TransferRelationListUiState(
     val payments: List<Item> = emptyList(),
+    val isLoading: Boolean = false,
 ) {
     data class Item(
         val name: String,
@@ -26,6 +29,12 @@ class TransferRelationListViewModel @Inject constructor(
     paymentQueryUseCase: PaymentQueryUseCase,
 ) : ViewModel() {
 
+    private val paymentsAsync = paymentQueryUseCase.loadPayments()
+        .map { Async.Success(it.toTransferRelationList()) }
+        .catch<Async<List<TransferRelationListUiState.Item>>> {
+            emit(Async.Error(R.string.load_error))
+        }
+
     /**
      * 更新用.
      */
@@ -34,12 +43,28 @@ class TransferRelationListViewModel @Inject constructor(
     /**
      * 読み取り専用.
      */
-    val uiState: StateFlow<TransferRelationListUiState> = _uiState.asStateFlow()
-    private val paymentsAsync = paymentQueryUseCase.loadPayments()
-        .map { Async.Success(it.toTransferRelationList()) }
-        .catch<Async<List<TransferRelationListUiState.Item>>> {
-            emit(Async.Error(R.string.load_error))
+    val uiState: StateFlow<TransferRelationListUiState> = paymentsAsync.map { paymentsAsync ->
+        when (paymentsAsync) {
+            is Async.Loading -> {
+                TransferRelationListUiState(isLoading = true)
+            }
+
+            is Async.Error -> {
+                // TODO エラーメッセージをキューに追加する
+                TransferRelationListUiState()
+            }
+
+            is Async.Success -> {
+                TransferRelationListUiState(
+                    payments = paymentsAsync.data
+                )
+            }
         }
+    }.stateIn(
+        scope = viewModelScope,
+        started = WhileUiSubscribed,
+        initialValue = TransferRelationListUiState(isLoading = true)
+    )
 
 
 }
