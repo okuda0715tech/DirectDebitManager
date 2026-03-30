c324c7d41abbbb91e426dc1fdcd7191d64e25600
奥田 智紘 <okuda0715tech@gmail.com>
2026-03-30 16:40:32 +0900

    feat: 支払情報選択画面にDBから取得したデータを表示できるようにした。

---
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_select/PaymentSelectMapper.kt
@@ -0,0 +1,15 @@
+package com.kurodai0715.directdebitmanager.ui.screen.payment_select
+
+import com.kurodai0715.directdebitmanager.data.source.local.PaymentEntityV2
+
+fun PaymentEntityV2.toPaymentSelect(): PaymentSelectUiState.Success.Item {
+    return PaymentSelectUiState.Success.Item(
+        id = id,
+        name = label,
+        selected = false,
+    )
+}
+
+fun List<PaymentEntityV2>.toPaymentSelect(): List<PaymentSelectUiState.Success.Item> {
+    return map { it.toPaymentSelect() }
+}
--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_select/PaymentSelectViewModel.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_select/PaymentSelectViewModel.kt
@@ -1,8 +1,16 @@
 package com.kurodai0715.directdebitmanager.ui.screen.payment_select
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kurodai0715.directdebitmanager.R
+import com.kurodai0715.directdebitmanager.domain.usecase.PaymentQueryUseCase
+import com.kurodai0715.directdebitmanager.ui.util.Async
+import com.kurodai0715.directdebitmanager.ui.util.WhileUiSubscribed
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 sealed interface PaymentSelectUiState {
@@ -21,10 +29,34 @@ sealed interface PaymentSelectUiState {
 
 @HiltViewModel
 class PaymentSelectViewModel @Inject constructor(
-
+    paymentQueryUseCase: PaymentQueryUseCase,
 ) : ViewModel() {
 
-    val uiState: StateFlow<PaymentSelectUiState> = TODO()
+    private val asyncPayments = paymentQueryUseCase.loadPayments()
+        .map { Async.Success(it.toPaymentSelect()) }
+        .catch<Async<List<PaymentSelectUiState.Success.Item>>> {
+            emit(Async.Error(R.string.load_error))
+        }
+
+    val uiState: StateFlow<PaymentSelectUiState> = asyncPayments.map { asyncPayments ->
+        when (asyncPayments) {
+            is Async.Loading -> {
+                PaymentSelectUiState.Loading
+            }
+
+            is Async.Error -> {
+                PaymentSelectUiState.Error(asyncPayments.errorMessage)
+            }
+
+            is Async.Success -> {
+                PaymentSelectUiState.Success(asyncPayments.data)
+            }
+        }
+    }.stateIn(
+        scope = viewModelScope,
+        started = WhileUiSubscribed,
+        initialValue = PaymentSelectUiState.Loading
+    )
 
     fun onClickItem(payment: PaymentSelectUiState.Success.Item) {
         TODO()
