346dfed6b345f548d5e1754b23eb2ed78dc9037a
奥田 智紘 <okuda0715tech@gmail.com>
2026-05-24 11:02:24 +0900

    feat: PaymentEditScreen を作成した。

---
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_edit/PaymentEditScreen.kt
@@ -0,0 +1,9 @@
+package com.kurodai0715.directdebitmanager.ui.screen.payment_edit
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PaymentEditScreen() {
+    Text("PaymentEditScreen")
+}
