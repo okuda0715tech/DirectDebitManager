92fa47b48815301bf73102339c973784a5bf6753
奥田 智紘 <okuda0715tech@gmail.com>
2026-05-25 12:39:02 +0900

    feat: 支払い情報の保存結果に応じてスナックバーを表示する処理を追加した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_edit/PaymentEditViewModel.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/payment_edit/PaymentEditViewModel.kt
@@ -2,11 +2,15 @@ package com.kurodai0715.directdebitmanager.ui.screen.payment_edit
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.kurodai0715.directdebitmanager.R
+import com.kurodai0715.directdebitmanager.domain.model.CreatePaymentResult
 import com.kurodai0715.directdebitmanager.domain.model.Payment
 import com.kurodai0715.directdebitmanager.domain.usecase.PaymentCommandUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -15,6 +19,10 @@ data class PaymentEditUiState(
     val id: Int,
     val name: String,
 )
+
+sealed class UiEvent {
+    data class ShowSnackbar(val messageRes: Int) : UiEvent()
+}
 
 @HiltViewModel
 class PaymentEditViewModel @Inject constructor(
@@ -25,19 +33,38 @@ class PaymentEditViewModel @Inject constructor(
 
     val uiState: StateFlow<PaymentEditUiState> = _uiState
 
+    /**
+     * 更新用.
+     */
+    private val _eventChannel = Channel<UiEvent>(Channel.BUFFERED)
+
+    /**
+     * 参照用.
+     */
+    val eventFlow = _eventChannel.receiveAsFlow()
+
     fun updateName(name: String) {
         _uiState.update { it.copy(name = name) }
     }
 
     fun onClickSave() {
-        val payment = uiState.value.toDomain()
+        viewModelScope.launch {
+            val payment = uiState.value.toDomain()
 
-        savePayment(payment)
+            val result = savePayment(payment)
+
+            when (result) {
+                CreatePaymentResult.Succeeded -> {
+                    _eventChannel.send(UiEvent.ShowSnackbar(R.string.common_save_successfully))
+                }
+
+                CreatePaymentResult.Failed ->
+                    _eventChannel.send(UiEvent.ShowSnackbar(R.string.common_save_failed))
+            }
+        }
     }
 
-    private fun savePayment(payment: Payment.InMemory) {
-        viewModelScope.launch {
-            paymentCommandUseCase.createPayment(payment)
-        }
+    private suspend fun savePayment(payment: Payment.InMemory): CreatePaymentResult {
+        return paymentCommandUseCase.createPayment(payment)
     }
 }
