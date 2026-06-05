ba3cbc6a7b9b4e49ff9824f3fda781a6909aa821
奥田 智紘 <okuda0715tech@gmail.com>
2026-06-05 16:23:20 +0900

    refactor: インポート文のエイリアス設定を整理した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/navigation/AppNavGraph.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/navigation/AppNavGraph.kt
@@ -25,7 +25,7 @@ import com.kurodai0715.directdebitmanager.ui.screen.payer_list.PayerListScreen
 import com.kurodai0715.directdebitmanager.ui.screen.payment_select.ViewModel as PaymentSelectViewModel
 import com.kurodai0715.directdebitmanager.ui.screen.source_edit.SourceEditScreen
 import com.kurodai0715.directdebitmanager.ui.screen.source_list.SourceListScreen
-import com.kurodai0715.directdebitmanager.ui.screen.transfer_relation_edit.TransferRelationEditViewModel
+import com.kurodai0715.directdebitmanager.ui.screen.transfer_relation_edit.TransferRelationEditViewModel as TransferRelationEditViewModel
 import com.kurodai0715.directdebitmanager.ui.screen.payment_edit.Screen as PaymentEditScreen
 import com.kurodai0715.directdebitmanager.ui.screen.payment_select.Screen as PaymentSelectScreen
 import com.kurodai0715.directdebitmanager.ui.screen.transfer_relation_edit.Screen as TransferRelationEditScreen
