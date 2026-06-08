e3a98e03f0488256fb945da89c65e30d79df89b6
奥田 智紘 <okuda0715tech@gmail.com>
2026-06-08 10:51:07 +0900

    refactor: プロパティ名を payeesV2 から payees に変更した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/transfer_relation_edit/ViewModel.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/transfer_relation_edit/ViewModel.kt
@@ -135,7 +135,7 @@ class ViewModel @Inject constructor(
             initialValue = UiState.Payer.Unassigned
         )
 
-    private val payeesV2 = paymentQueryUseCase.loadPaymentsBy(paymentId)
+    private val payees = paymentQueryUseCase.loadPaymentsBy(paymentId)
         .map { it.toUiPayeesV2() }
 
     private val dialog = MutableStateFlow<UiState.Dialog>(UiState.Dialog.None)
@@ -143,7 +143,7 @@ class ViewModel @Inject constructor(
     /**
      * UI で必要となる全ての状態.
      */
-    val uiState: StateFlow<UiState> = combine(payment, payer, payeesV2, dialog)
+    val uiState: StateFlow<UiState> = combine(payment, payer, payees, dialog)
     { payment, payer, payees, dialog ->
         UiState(payment = payment, payer = payer, payees = payees, dialog = dialog)
     }.stateIn(
