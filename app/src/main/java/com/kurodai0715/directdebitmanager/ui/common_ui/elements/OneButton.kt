d0338c5ec617e8aab83d7ebce4426b9a71fe111f
奥田 智紘 <okuda0715tech@gmail.com>
2026-06-09 11:45:17 +0900

    refactor: OneButton からパディングを削除した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/common_ui/elements/OneButton.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/common_ui/elements/OneButton.kt
@@ -8,7 +8,6 @@ package com.kurodai0715.directdebitmanager.ui.common_ui.elements
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,7 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.kurodai0715.directdebitmanager.R
-import com.kurodai0715.directdebitmanager.ui.theme.LayoutTokens
 
 @Composable
 fun OneButton(
@@ -25,9 +23,7 @@ fun OneButton(
 ) {
 
     Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(LayoutTokens.sectionSpacing),
+        modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
         Button(onClick = { onClick() }) {
