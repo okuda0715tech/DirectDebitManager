f12a41b16e2b3f3bc2173fe542004f4cde877e25
奥田 智紘 <okuda0715tech@gmail.com>
2026-06-10 12:05:31 +0900

    feat: 振替関係一覧画面にデータがない場合の表示を追加した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/transfer_relation_list/Screen.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/screen/transfer_relation_list/Screen.kt
@@ -3,16 +3,24 @@ package com.kurodai0715.directdebitmanager.ui.screen.transfer_relation_list
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -20,6 +28,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kurodai0715.directdebitmanager.R
 import com.kurodai0715.directdebitmanager.ui.common_ui.elements.OneButton
 import com.kurodai0715.directdebitmanager.ui.common_ui.screens.BodyBottomButtonLayout
+import com.kurodai0715.directdebitmanager.ui.theme.ICON_EX_LARGE_SIZE
 import com.kurodai0715.directdebitmanager.ui.theme.LayoutTokens
 import com.kurodai0715.directdebitmanager.ui.util.debouncedClick
 
@@ -51,7 +60,7 @@ fun Contents(
     BodyBottomButtonLayout(
         modifier = modifier,
         body = {
-            Body(onClickItem, uiState)
+            Body(onClickAdd, onClickItem, uiState)
         },
         bottomButton = {
             OneButton(
@@ -64,6 +73,7 @@ fun Contents(
 
 @Composable
 fun Body(
+    onClickEmptyMessage: () -> Unit,
     onClickItem: (Int) -> Unit,
     uiState: TransferRelationListUiState,
 ) {
@@ -78,12 +88,41 @@ fun Body(
         }
 
         is TransferRelationListUiState.Success -> {
-            LazyColumn {
-                items(uiState.payments) { item ->
-                    ListItem(item, onClickItem = { onClickItem(item.id) })
+            val payments = uiState.payments
+            if (payments.isEmpty()) {
+                EmptyMessage(onClickEmptyMessage)
+            } else {
+                LazyColumn {
+                    items(uiState.payments) { item ->
+                        ListItem(item, onClickItem = { onClickItem(item.id) })
+                    }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun EmptyMessage(onClickEmptyMessage: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .clickable(true) {
+                debouncedClick(onClickEmptyMessage)
+            },
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.outline_wand_stars_24),
+            contentDescription = stringResource(R.string.empty_icon_description),
+            modifier = Modifier.size(ICON_EX_LARGE_SIZE),
+            tint = MaterialTheme.colorScheme.primary,
+        )
+
+        Spacer(modifier = Modifier.size(LayoutTokens.sectionSpacing))
+
+        Text(text = stringResource(R.string.empty_message_label))
     }
 }
 
