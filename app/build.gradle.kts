92f6cf50ade765e48a0a207ee6f29dc8c26f5d09
奥田 智紘 <okuda0715tech@gmail.com>
2026-06-11 22:36:17 +0900

    build: compileSdk と targetSdk を 36 に更新した。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.kurodai0715.directdebitmanager"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.kurodai0715.directdebitmanager"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
