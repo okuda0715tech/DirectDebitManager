a2dfae8623a1fa28958782ad7e3f2c2a5d4b67db
奥田 智紘 <okuda0715tech@gmail.com>
2026-06-18 19:06:40 +0900

    ui: BasicTextField のカーソル色を設定した。

--- app/src/main/java/com/kurodai0715/directdebitmanager/ui/common_ui/elements/BasicTextField.kt
+++ app/src/main/java/com/kurodai0715/directdebitmanager/ui/common_ui/elements/BasicTextField.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
 import com.kurodai0715.directdebitmanager.ui.theme.TEXT_FIELD_MIN_HEIGHT
 
@@ -25,6 +26,7 @@ fun DefaultBasicTextField(
     BasicTextField(
         value = text,
         onValueChange = onTextChanged,
+        cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
         textStyle = LocalTextStyle.current.copy(
             color = MaterialTheme.colorScheme.onSurface,
             fontSize = MaterialTheme.typography.bodyLarge.fontSize
